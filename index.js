073ef9be5428d03fd0609c0ee6f2e6b9a97b1150
4423 <hanamiya4423@gmail.com>
2019-07-27 18:22:26 +0900

    fix

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const port = process.env.PORT;
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Hello World!");
+  res.end("Hello Azure!");
 }).listen(port);
